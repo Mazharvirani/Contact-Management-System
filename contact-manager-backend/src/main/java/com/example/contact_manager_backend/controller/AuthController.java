ac21c2d5cde0ddaac38def68054ce2f9f9cb0918
Mazharvirani <virani.mazher@gmail.com>
2026-05-02 01:29:59 +0500

    Slf4J implementation

--- contact-manager-backend/src/main/java/com/example/contact_manager_backend/controller/AuthController.java
+++ contact-manager-backend/src/main/java/com/example/contact_manager_backend/controller/AuthController.java
@@ -7,10 +7,13 @@ import com.example.contact_manager_backend.entity.user;
 import com.example.contact_manager_backend.service.UserService;
 
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     private final UserService userService;
     private final JwtUtil jwtUtil;
@@ -22,19 +25,24 @@ public class AuthController {
 
     @PostMapping("/register")
     public user register(@RequestBody RegisterRequest request) {
-        return userService.register(request);
+        log.info("New User Registered attempt: {}",request.email);
+        user registered = userService.register(request);
+        log.info("User registered Successfully: {}",request.email);
+        return registered;
     }
 
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request) {
-
+        log.info("Login attempt for: {}", request.identifier);
         boolean success = userService.login(request);
-
-        if (success) {
+        if(success){
+            log.info("login successful for: {}",request.identifier);
             String email = userService.getEmailFromIdentifier(request.identifier);
             return jwtUtil.generateToken(email);
-        } else {
-            return "Invalid Credentials";
+        }else{
+            log.warn("login failed for: {}",request.identifier);
+            return "invalid credentials";
         }
+
     }
 }
